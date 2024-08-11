77b08c026438efa723191a8e837c3e1cc97fe4ea
Jesse Monks <heateddiv@gmail.com>
2024-08-11 12:17:41 -0400

    moving element creation to helper functions. Still working.

--- imageQuery.js
+++ imageQuery.js
@@ -11,13 +11,12 @@ javascript:(function () {
     const imgNodes = document.querySelectorAll("img:not([role])");
     const nonImgNodes = document.querySelectorAll("[role=img]");
 
-    const buildElement = (type, color, className = 'bookMarklet') => {
+    const buildElement = (type, color, text, className = 'bookMarklet', backgroundColor = 'rgb(255, 255, 255, .6)') => {
         const newElement = document.createElement(`${type}`);
 
         newElement.className = `${className}`;
-        newElement.textContent = 'Pass';
-        newElement.style.backgroundColor = 'white';
-        newElement.style.opacity = '.8';
+        newElement.textContent = `${text}`;
+        newElement.style.backgroundColor = `${backgroundColor}`;
         newElement.style.color = `${color}`;
         newElement.style.border = '2px, solid, Green';
         newElement.style.padding = '3px';
@@ -27,15 +26,14 @@ javascript:(function () {
         return newElement;
     };
 
-    const buildPopOver = (type, color, text, className = 'bookMarklet') => {
+    const buildPopOver = (type, color, text,  className = 'bookMarklet', backgroundColor = 'rgb(255, 255, 255, .6)') => {
         const newElement = document.createElement(`${type}`);
 
         newElement.className = `${className}`;
         newElement.style.textContent = `${text}`;
         newElement.style.position = 'absolute';
         newElement.style.top = `${imgFailElement.getBoundingClientRect().bottom}px`;
-        newElement.style.backgroundColor = 'white';
-        newElement.style.opacity = '.8';
+        newElement.style.backgroundColor = `${backgroundColor}`;
         newElement.style.color = `${color}`;
         newElement.style.border = '2px, solid, red';
         newElement.style.padding = '3px';
@@ -53,8 +51,26 @@ javascript:(function () {
         let containsAriaLabel = imgNodes[element].getAttribute('aria-label');
 
         if ((containsAriaHidden === 'true') || containsAltText || containsAriaLabel) {
-            let passElement = buildElement('div', 'green');
-        console.log(passElement);
+            let passElement = buildElement('div', 'green', 'Passed');
+            let ariaHiddenElement = buildElement('p', 'green', 'aria-hidden=' + containsAriaHidden, 'passed-popOver');
+            let altElement = buildElement('p', 'green', 'alt=' + containsAltText, 'passed-popOver');
+            let ariaLabelElement = buildElement('p', 'green', 'aria-label=' + containsAriaLabel, 'passed-popOver');
+
+            passElement.addEventListener('mouseover', () => {
+                passElement.appendChild(ariaHiddenElement);
+                passElement.appendChild(altElement);
+                passElement.appendChild(ariaLabelElement);
+            });
+
+            passElement.addEventListener('mouseout', () => {
+                let elementsToRemove = document.querySelectorAll('.passed-popOver');
+    
+                for (const element in elementsToRemove) {
+                    console.log(elementsToRemove[element]);
+                    elementsToRemove[element].remove();
+    
+                }
+            });
 
             imgNodes[element].before(passElement);
             continue;
@@ -90,55 +106,44 @@ javascript:(function () {
         let containsAriaLabel = nonImgNodes[element].getAttribute('aria-label');
 
         if ((containsAriaHidden === 'true') || containsAltText || containsAriaLabel) {
-            let passElement = document.createElement('div');
+            let passElement = buildElement('div', 'green', 'Passed', 'bookMarklet');
+            let ariaHiddenElement = buildElement('p', 'green', 'aria-hidden=' + containsAriaHidden, 'passed-popOver');
+            let altElement = buildElement('p', 'green', 'alt=' + containsAltText, 'passed-popOver');
+            let ariaLabelElement = buildElement('p', 'green', 'aria-label=' + containsAriaLabel, 'passed-popOver');
 
-            passElement.className = 'bookMarklet';
-            passElement.textContent = 'Passed';
-            passElement.style.backgroundColor = 'white';
-            passElement.style.opacity = '.8';
-            passElement.style.color = 'Green';
-            passElement.style.border = '2px, solid, Green';
-            passElement.style.padding = '3px';
-            passElement.style.borderRadius = '3px';
-            passElement.style.width = 'max-content';
+            passElement.addEventListener('mouseover', () => {
+                passElement.appendChild(ariaHiddenElement);
+                passElement.appendChild(altElement);
+                passElement.appendChild(ariaLabelElement);
+            });
+
+            passElement.addEventListener('mouseout', () => {
+                let elementsToRemove = document.querySelectorAll('.passed-popOver');
+    
+                for (const element in elementsToRemove) {
+                    console.log(elementsToRemove[element]);
+                    elementsToRemove[element].remove();
+    
+                }
+            });
     
             nonImgNodes[element].before(passElement);
             continue;
         }
 
-        let pseudoElement = document.createElement('div');
+        let failElement = buildElement('div', 'red', 'X', 'bookMarklet');
+        let ariaHiddenElement = buildElement('p', 'red', 'aria-hidden=' + containsAriaHidden, 'failed-popOver');
+        let altElement = buildElement('p', 'red', 'alt=' + containsAltText, 'failed-popOver');
+        let ariaLabelElement = buildElement('p', 'red', 'aria-label=' + containsAriaLabel, 'failed-popOver');
 
-        pseudoElement.className = 'bookMarklet';
-        pseudoElement.textContent = 'X no alt text found';
-        pseudoElement.style.position = 'relative';
-        pseudoElement.style.backgroundColor = 'white';
-        pseudoElement.style.opacity = '.8';
-        pseudoElement.style.color = 'red';
-        pseudoElement.style.border = '2px, solid, red';
-        pseudoElement.style.padding = '3px';
-        pseudoElement.style.borderRadius = '3px';
-
-        pseudoElement.addEventListener('mouseover', () => {
-            let infobox = document.createElement('div');
-
-            infobox.className = 'infoBox';
-
-            infobox.textContent = 'test 2';
-
-            infobox.style.position = 'absolute';
-            infobox.style.top = `${pseudoElement.getBoundingClientRect().bottom}px`;
-            infobox.style.backgroundColor = 'white';
-            infobox.style.opacity = '.8';
-            infobox.style.color = 'red';
-            infobox.style.border = '2px, solid, red';
-            infobox.style.padding = '3px';
-            infobox.style.borderRadius = '3px';
-            infobox.style.width = 'max-content';
-            pseudoElement.after(infobox);
+        failElement.addEventListener('mouseover', () => {
+            failElement.appendChild(ariaHiddenElement);
+            failElement.appendChild(altElement);
+            failElement.appendChild(ariaLabelElement);
         });
 
-        pseudoElement.addEventListener('mouseout', () => {
-            let infobox = document.querySelectorAll('.infoBox');
+        failElement.addEventListener('mouseout', () => {
+            let infobox = document.querySelectorAll('.failed-popOver');
 
             for (const element in infobox) {
                 console.log(infobox[element]);
@@ -147,7 +152,7 @@ javascript:(function () {
             }
         });
 
-        nonImgNodes[element].before(pseudoElement);
+        nonImgNodes[element].before(failElement);
         console.log('Not an image element', nonImgNodes[element]);
 }
 })();
