e2db65393fb015f9e2c8433e5058b5607d2a5323
cavelemon <bypastorelli@yahoo.com>
2024-08-13 18:04:13 -0400

    Baseline focus recognition
    Works with the interactive elements defined in the queryselector, need to work on applying styles and logging

--- findMyFocus.js
+++ findMyFocus.js
@@ -1,3 +1,22 @@
 javascript:(function () {
-    console.log("Hello world...");
+    const existingNode = document.querySelector(".bookMarklet");
+    if (existingNode) {
+        existingNode.classList.remove("bookMarklet");
+        isFocusEnabled = false;
+        return;
+    }
+    
+    const focusableNodes = document.querySelectorAll('a, button, input, select');
+    var currentlyFocusedElement;
+
+    window.addEventListener('keydown', function(e){
+        if(e.key == "Tab" && focusableNodes.length){
+            for (const focusableNode of focusableNodes) {
+                currentlyFocusedElement = document.activeElement;
+                if(currentlyFocusedElement == focusableNode){
+                    console.log("focused element:" + focusableNode.tagName);
+                }
+            }
+        }
+    });
 })();
