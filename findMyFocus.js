0ae1cba56df31d77c0b8d2283c730c278f963957
cavelemon <bypastorelli@yahoo.com>
2024-08-20 15:29:14 -0400

    Logic to remove class on blur
    Added event listener to handle the removal of the class "bookMarklet" given on focus. This will be part of the logic in applying the focus style to only the currently focused element

--- findMyFocus.js
+++ findMyFocus.js
@@ -7,15 +7,26 @@ javascript:(function () {
     }
     
     const focusableNodes = document.querySelectorAll('a, button, input, textarea, select, div[role=button], div[role=link], div[role=tab], div[role=tabpanel]');
-    var currentlyFocusedElement;
 
     window.addEventListener('keyup', function(e){
-        if(e.key == "Tab" && focusableNodes.length){
+        if (e.key == "Tab" && focusableNodes.length){
             for (const fn of focusableNodes) {
                 if(document.activeElement == fn){
+                    fn.className = 'bookMarklet';
                     console.log("Focused element: ", fn);
                 }
             }
         }
     });
+    
+    window.addEventListener('keydown', function(e){
+        if (e.key == "Tab"){
+            if (e.target.getAttribute("class") === 'bookMarklet'){
+                e.target.removeAttribute('class');
+                console.log("removed class from", e.target);
+            }
+        }
+    });
+
+
 })();
